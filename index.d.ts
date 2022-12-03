5e5fc434971f17aeba2acc4e9d36ee484a0efe4a
E0SelmY4V-PC <2507917746@qq.com>
2022-12-04 00:47:31 +0800

    ver: 1.1.1完善声明

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+declare function exp(exp: any, ...mods: any[]): void
+declare function merObj(a: any, b: any): void
+export = exp
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 "use strict";
 
-/**@type {(a:any,b:any)=>void} */
 function merObj(a, b) {
 	for (var i in b) typeof a[i] === 'object' && typeof b[i] === 'object'
 		? merObj(a[i], b[i])
 		: (a[i] = b[i]);
 }
 
-/**@type {(exp:any,...mods:any[])=>void} */
-module.exports = function () {
+function exp() {
 	var exp = arguments[0];
 	for (var i = arguments.length - 1; i > 0; --i) merObj(exp, arguments[i]);
-};
+}
+module.exports = exp;
