722b05bf38b422113cdaca844cdfc3eccbcdd626
E0SelmY4V-PC <2507917746@qq.com>
2022-12-04 00:39:21 +0800

    ver: 1.1.0可以merObj了

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 "use strict";
 
+/**@type {(a:any,b:any)=>void} */
+function merObj(a, b) {
+	for (var i in b) typeof a[i] === 'object' && typeof b[i] === 'object'
+		? merObj(a[i], b[i])
+		: (a[i] = b[i]);
+}
+
 /**@type {(exp:any,...mods:any[])=>void} */
 module.exports = function () {
 	var exp = arguments[0];
-	for (var i = arguments.length - 1; i > 0; --i)
-		for (var j in arguments[i])
-			exp[j] = arguments[i][j];
+	for (var i = arguments.length - 1; i > 0; --i) merObj(exp, arguments[i]);
 };
