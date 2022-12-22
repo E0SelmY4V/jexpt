be8e05219d502ebc9dd992fa2d25b6d295d42266
E0SelmY4V-PC <2507917746@qq.com>
2022-12-22 14:14:39 +0800

    Add 间接性引入

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,17 @@
-declare function exp(exp: any, ...mods: any[]): void
 declare function merObj(a: any, b: any): void
+interface InitlessReged<M> {
+	exp: M
+	req: any
+}
+interface DestObj {
+	name: string
+	dest: string
+}
+interface InitedReged<M> extends InitlessReged<M> {
+	req: (id: string) => any
+	pack: (...dests: (string | DestObj)[]) => InitedReged<M>
+	merge: (...ids: string[]) => InitedReged<M>
+}
+declare function exp<M>(exp: M, req: (id: string) => any): InitedReged<M>
+declare function exp<M>(exp: M, ...mods: any[]): InitlessReged<M>
 export = exp
--- index.js
+++ index.js
@@ -6,8 +6,29 @@ function merObj(a, b) {
 		: (a[i] = b[i]);
 }
 
+function Reged(exp, req) {
+	this.exp = exp;
+	this.req = req;
+}
+Reged.prototype = {
+	exp: null,
+	req: function () { return {}; },
+	pack: function () {
+		for (var i = arguments.length - 1; i >= 0; --i)
+			typeof arguments[i] === 'string'
+				? this.exp[arguments[i]] = this.req('./' + arguments[i])
+				: this.exp[arguments[i].name] = this.req('./' + arguments[i].dest);
+		return this;
+	},
+	merge: function () {
+		for (var i = arguments.length - 1; i >= 0; --i) merObj(this.exp, this.req('./' + arguments[i]));
+		return this;
+	}
+};
+
 function exp() {
 	var exp = arguments[0];
 	for (var i = arguments.length - 1; i > 0; --i) merObj(exp, arguments[i]);
+	return new Reged(exp, arguments[1]);
 }
 module.exports = exp;
