e8f2129d4cf77237762b81f8f478fa5e534de4e0
E0SelmY4V-PC <2507917746@qq.com>
2022-12-22 16:55:21 +0800

    Ver 1.2.2 修复声明文件

--- index.d.ts
+++ index.d.ts
@@ -7,6 +7,6 @@ declare class Reged<M> {
 	pack: (...dests: (string | DestObj)[]) => Reged<M>
 	merge: (...ids: string[]) => Reged<M>
 }
-declare function exp<M>(exp: M, req: { (id: any): any, cache: {} }): Reged<M>
+declare function exp<M>(exp: M, req: { (id: string): any, cache: {} }): Reged<M>
 declare function exp<M>(exp: M, ...mods: any[]): void
 export = exp
